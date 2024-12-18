6ea1d21a5a505cac61168e17d39a113c33dcb0a1
shibacoinppc <137075650+shibacoinppc@users.noreply.github.com>
2024-12-18 17:14:52 -0500

    Update clientversion.cpp

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both bitcoind and bitcoin-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("pepetoshi");
+const std::string CLIENT_NAME("shibatoshi");
 
 /**
  * Client version number
